b641c12e3ccb2738a64927fd9b2036cf8342862f
abhijitramesh <abhijitramesh2k@gmail.com>
2019-04-26 20:57:11 +0530

    added example program

---
+++ Objects and Classes/Hedgehog.java
@@ -0,0 +1,20 @@
+class Hedgehog{ // Class named Hedgehog
+
+
+    // Instance Variables
+    int id;
+    String name;
+
+    // main method inside Hedgehog class
+    public static void main(String args[]){
+
+        //Creating an instance
+
+        Hedgehog h1 = new Hedgehog();   //h1 is an object created from class Headgehog
+
+        //accesing memeber variables of the object h1
+        System.out.println(h1.id);      
+        System.out.println(h1.name);
+
+    }
+}
