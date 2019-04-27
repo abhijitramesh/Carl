5e53e365c84a380c671180611da1016a68621172
abhijitramesh <abhijitramesh2k@gmail.com>
2019-04-27 18:38:58 +0530

    added program which has main method outside class

---
+++ Objects and Classes/Bank.java
@@ -0,0 +1,18 @@
+class CarlBank{
+
+    int customerId;
+    String customerName;
+
+}
+
+
+public class Bank{
+public static void main(String args[]){
+    CarlBank b1 = new CarlBank();
+
+    System.out.println(b1.customerId);
+
+}
+
+}
+
